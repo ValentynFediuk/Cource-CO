82f5c0f17b78247f85b903e960a61a4fa6b26ca8
Valentyn <sigano802@gmail.com>
2021-12-15 15:45:39 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  $('#myDropdown').toggleClass('show');
+  $('#overlay').toggleClass('show-overlay');
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
