ced735bf2d955558ce226d71e50fc98fdd77e7b4
Valentyn <sigano802@gmail.com>
2021-12-16 17:12:25 +0200

    hero fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
-function myFunction() {
-  $('#myDropdown').toggleClass('show');
+function productClick() {
+  $('#myDropdown').toggleClass('show-dropdown');
   $('#overlay').toggleClass('show-overlay');
+  $('#product-dropdown').toggleClass('menu__item--active');
+  $('#underline').toggleClass('menu__link-underline--active');
 }
 function searchClick() {
   $('#search-input').addClass('buttons__search-input--active');
@@ -12,15 +14,11 @@ function closeClick() {
 }
 function bookingClick() {
   $('#booking-wrapper').toggleClass('dropdown-content__icon-wrapper--active');
+  $('#booking-title').toggleClass('dropdown-content__item-title--active');
+  $('#dropdown-right').toggleClass('dropdown-content__right--active');
 }
 function dropdownItemClick() {
   $('#dropdown-item-wrapper').toggleClass('dropdown-content__item-wrapper--active');
   $('#booking-icon').toggleClass('dropdown-content__item-icon--active');
   $('#item-arrow-right').toggleClass('dropdown-content__item-arrow-right--active');
-
 }
-
-// function resourcesDropdown() {
-//   $('#resourcesDropdown').toggleClass('show');
-//   $('#overlay').toggleClass('show-overlay');
-// }
