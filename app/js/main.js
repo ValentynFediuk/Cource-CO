c05da9b998ccfe88e8815449caf3a353703232bd
Valentyn <sigano802@gmail.com>
2021-12-16 14:26:32 +0200

    dropdown

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,14 @@ function closeClick() {
   $('#search-input').removeClass('buttons__search-input--active');
   $('#search-close').removeClass('buttons__search-close--active');
 }
+function bookingClick() {
+  $('#booking-wrapper').toggleClass('dropdown-content__icon-wrapper--active');
+}
+function dropdownItemClick() {
+  $('#dropdown-item-wrapper').toggleClass('dropdown-content__item-wrapper--active');
+  $('#booking-icon').toggleClass('dropdown-content__item-icon--active');
 
+}
 
 // function resourcesDropdown() {
 //   $('#resourcesDropdown').toggleClass('show');
